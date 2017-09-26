3f2df1e3baabcfd02e9b2abf2aead0a99f243576
Dan D'Avella <drdavella@gmail.com>
2017-09-26 10:17:21 -0400

    First commit of new meta package...
    It currently pulls dependencies on unpublished pytest plugins from
    github.

---
+++ README.rst
@@ -0,0 +1,7 @@
+========
+Overview
+========
+
+This is a meta package that pulls in dependencies on each of the new standalone
+pytest plugins that are used by the astropy core and affiliated packages. It
+is currently WIP.
---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- encoding: utf-8 -*-
+
+import io
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pytest-astropy',
+    version='0.1.0',
+    license='BSD',
+    description='Meta-package containing dependencies for testing',
+    author='The Astropy Developers',
+    author_email='astropy.team@gmail.com',
+    url='https://astropy.org',
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities',
+    ],
+    keywords=[ 'pytest', 'py.test', 'remotedata', 'openfiles', 'doctestplus' ],
+    python_requires='>=2.7',
+    install_requires=[
+        'pytest>=2.8.0',
+        'pytest-doctestplus',
+        'pytest-remotedata',
+        'pytest-openfiles'
+    ],
+    # These links to github are temporary until the packages are available on pypi
+    dependency_links =[
+        'git+https://github.com/astropy/pytest-doctestplus.git#egg=pytest-doctestplus',
+        'git+https://github.com/astropy/pytest-remotedata.git#egg=pytest-remotedata',
+        'git+https://github.com/astropy/pytest-openfiles.git#egg=pytest-openfiles'
+    ],
+)
