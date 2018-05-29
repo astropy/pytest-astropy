505477a0ca6e605e2de1e18fb621d1c9a75fe5ba
Daniel D'Avella <ddavella@stsci.edu>
2018-05-29 16:39:18 -0400

    Update setup.py and CHANGES.rst for next release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def readme():
 
 setup(
     name='pytest-astropy',
-    version='0.4.0',
+    version='0.4.1.dev',
     license='BSD',
     description='Meta-package containing dependencies for testing',
     long_description=readme(),
