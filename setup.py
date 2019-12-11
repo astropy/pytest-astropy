c72a8aa755274068e8ffe41ec42528fb9c5eac0f
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2019-12-11 09:26:23 -0700

    MNT: Update setup as recommended by @Zac-HD

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
     name='pytest-astropy',
-    version='0.7.0',
+    version='0.8.0.dev',
     license='BSD',
     description='Meta-package containing dependencies for testing',
     long_description=readme(),
@@ -22,34 +22,34 @@ setup(
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # complete classifier list:
+        # http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 3 - Alpha',
         'Framework :: Pytest',
+        'Framework :: Hypothesis',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Software Development :: Testing',
         'Topic :: Utilities',
     ],
-    keywords=[ 'pytest', 'py.test', 'remotedata', 'openfiles', 'doctestplus' ],
-    python_requires='>=2.7',
+    keywords=['pytest', 'py.test', 'remotedata', 'openfiles', 'doctestplus'],
+    python_requires='>=3.6',
     install_requires=[
-        'pytest>=3.1.0',
+        'pytest>=4.6',
         'pytest-doctestplus>=0.2.0',
         'pytest-remotedata>=0.3.1',
         'pytest-openfiles>=0.3.1',
         'pytest-astropy-header',
         # Do not include as dependency until CI issues can be worked out
-        #'pytest-mpl',
+        # 'pytest-mpl',
         'pytest-arraydiff>=0.1',
-        'hypothesis'
+        # Bump hypothesis to >=5.0 in early Jan 2020
+        'hypothesis>=4.53'
     ]
 )
