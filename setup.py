109bdfc9d6cfc548eea4b64d928b0177d6158b82
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2021-12-31 00:01:53 -0500

    MNT: Stop using distutils

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python
 
-import sys
-import setuptools
-from distutils.version import LooseVersion
 from setuptools import setup
-
-# Setuptools 30.3.0 or later is needed for setup.cfg options to be used
-if LooseVersion(setuptools.__version__) < LooseVersion('30.3.0'):
-    sys.stderr.write("ERROR: pytest-astropy requires setuptools 30.3.0 or "
-                     "later (found {0})".format(setuptools.__version__))
-    sys.exit(1)
 
 setup(use_scm_version=True)
